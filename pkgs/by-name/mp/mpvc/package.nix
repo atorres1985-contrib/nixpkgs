2bf0fc81a1c1a71d37401910a0bae2643babf7b8
Anderson Torres <torres.anderson.85@protonmail.com>
2024-02-05 20:33:41 -0300

    mpvc: 1.3 -> 1.5-web
    Jumped 1.4 since it does not have a Makefile.
    
    (What if we ignored Makefile completely?...)

--- pkgs/by-name/mp/mpvc/package.nix
+++ pkgs/by-name/mp/mpvc/package.nix
@@ -9,13 +9,13 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "mpvc";
-  version = "1.3";
+  version = "1.5-web";
 
   src = fetchFromGitHub {
     owner = "gmt4";
     repo = "mpvc";
     rev = finalAttrs.version;
-    hash = "sha256-wPETEG0BtNBEj3ZyP70byLzIP+NMUKbnjQ+kdvrvK3s=";
+    hash = "sha256-yNPTocqOJGCh/yLqa/3dpxhh5U2pmr6hQYu9l91D5Lg=";
   };
 
   postPatch = ''
@@ -47,7 +47,9 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     mkdir -p $out $doc/share/doc/
   '';
 
+  # This is not ArchLinux (TM), we don't need to store licensing files...
   postFixup = ''
+    rm -r $out/share/
     wrapProgram $out/bin/mpvc \
       --prefix PATH : "${lib.makeBinPath [ mpv socat ]}"
   '';
@@ -56,7 +58,8 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     homepage = "https://gmt4.github.io/mpvc/";
     description = "A mpc-like control interface for mpv";
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ AndersonTorres neeasade ];
+    mainProgram = "mpvc";
+    maintainers = with lib.maintainers; [ AndersonTorres ];
     platforms = lib.platforms.linux;
   };
 })
