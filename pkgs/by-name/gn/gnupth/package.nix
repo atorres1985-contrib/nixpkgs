aa309aa5c1ff35473e5672358d2ea0e9bfe6cd30
Anderson Torres <torres.anderson.85@protonmail.com>
2024-03-28 01:10:32 -0300

    gnupth: adopt and refactor
    - finalAttrs design pattern
    - meta.longDescription

--- pkgs/by-name/gn/gnupth/package.nix
+++ pkgs/by-name/gn/gnupth/package.nix
@@ -1,12 +1,15 @@
-{ lib, stdenv, fetchurl }:
+{ lib
+, fetchurl
+, stdenv
+}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "pth";
   version = "2.0.7";
 
   src = fetchurl {
-    url = "mirror://gnu/pth/pth-${version}.tar.gz";
-    sha256 = "0ckjqw5kz5m30srqi87idj7xhpw6bpki43mj07bazjm2qmh3cdbj";
+    url = "mirror://gnu/pth/pth-${finalAttrs.version}.tar.gz";
+    hash = "sha256-cjU2YMWiyq/WAbIOEuddhl/Yj2zxoIizBqOWPwvHcjI=";
   };
 
   preConfigure = lib.optionalString stdenv.isAarch32 ''
@@ -22,11 +25,33 @@ stdenv.mkDerivation rec {
   #     pth_uctx.c:31:10: fatal error: pth_p.h: No such file
   enableParallelBuilding = false;
 
-  meta = with lib; {
-    description = "The GNU Portable Threads library";
-    mainProgram = "pth-config";
+  meta = {
     homepage = "https://www.gnu.org/software/pth";
-    license = licenses.lgpl21Plus;
-    platforms = platforms.all;
+    description = "The GNU Portable Threads library";
+    longDescription = ''
+      Pth is a very portable POSIX/ANSI-C based library for Unix platforms which
+      provides non-preemptive priority-based scheduling for multiple threads of
+      execution (aka `multithreading') inside event-driven applications. All
+      threads run in the same address space of the server application, but each
+      thread has it's own individual program-counter, run-time stack, signal
+      mask and errno variable.
+
+      The thread scheduling itself is done in a cooperative way, i.e., the
+      threads are managed by a priority- and event-based non-preemptive
+      scheduler. The intention is that this way one can achieve better
+      portability and run-time performance than with preemptive scheduling. The
+      event facility allows threads to wait until various types of events occur,
+      including pending I/O on filedescriptors, asynchronous signals, elapsed
+      timers, pending I/O on message ports, thread and process termination, and
+      even customized callback functions.
+
+      Additionally Pth provides an optional emulation API for POSIX.1c threads
+      ("Pthreads") which can be used for backward compatibility to existing
+      multithreaded applications.
+    '';
+    license = lib.licenses.lgpl21Plus;
+    mainProgram = "pth-config";
+    maintainers = with lib.maintainers; [ AndersonTorres ];
+    platforms = lib.platforms.all;
   };
-}
+})
