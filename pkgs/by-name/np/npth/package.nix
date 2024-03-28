e254cd8f321e34053f4cce413bbc26bc3819dc28
Anderson Torres <torres.anderson.85@protonmail.com>
2024-03-28 01:15:16 -0300

    npth: adopt and refactor
    - finalAttrs design pattern
    - meta.longDescription

--- pkgs/by-name/np/npth/package.nix
+++ pkgs/by-name/np/npth/package.nix
@@ -1,15 +1,22 @@
-{ lib, stdenv, fetchurl, fetchpatch, autoreconfHook, pkgsCross }:
+{ lib
+, autoreconfHook
+, fetchpatch
+, fetchurl
+, pkgsCross
+, stdenv
+}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "npth";
   version = "1.7";
 
   src = fetchurl {
-    url = "mirror://gnupg/npth/npth-${version}.tar.bz2";
-    sha256 = "sha256-hYn1aTe3XOM7KNMS/MvzArO3HsPzlF/eaqp0AnkUrQU=";
+    url = "mirror://gnupg/npth/npth-${finalAttrs.version}.tar.bz2";
+    hash = "sha256-hYn1aTe3XOM7KNMS/MvzArO3HsPzlF/eaqp0AnkUrQU=";
   };
 
   patches = [
+    # Fix INSERT_EXPOSE_RWLOCK_API for musl C library
     (fetchpatch {
       name = "musl.patch";
       url = "https://git.gnupg.org/cgi-bin/gitweb.cgi?p=npth.git;a=patch;h=417abd56fd7bf45cd4948414050615cb1ad59134";
@@ -25,9 +32,9 @@ stdenv.mkDerivation rec {
     musl = pkgsCross.musl64.npth;
   };
 
-  meta = with lib; {
+  meta = {
+    homepage = "https://gnupg.org/software/npth/index.html";
     description = "The New GNU Portable Threads Library";
-    mainProgram = "npth-config";
     longDescription = ''
       This is a library to provide the GNU Pth API and thus a non-preemptive
       threads implementation.
@@ -37,8 +44,9 @@ stdenv.mkDerivation rec {
       compatible to GNU Pth.  Experience with a Windows Pth emulation showed
       that this is a solid way to provide a co-routine based framework.
     '';
-    homepage = "http://www.gnupg.org";
-    license = licenses.lgpl3;
-    platforms = platforms.all;
+    license = lib.licenses.lgpl3;
+    mainProgram = "npth-config";
+    maintainers = with lib.maintainers; [ AndersonTorres ];
+    platforms = lib.platforms.all;
   };
-}
+})
