11162e7f7454f327b43e0abd5290762fcd0ea090
Anderson Torres <torres.anderson.85@protonmail.com>
2024-05-01 23:32:37 -0300

    mpvc: refactor
    - use stdenvNoCC
    - split outputs
    - add mpv to buildInputs
    - add AndersonTorres as maintainer

--- pkgs/by-name/mp/mpvc/package.nix
+++ pkgs/by-name/mp/mpvc/package.nix
@@ -1,32 +1,62 @@
-{ lib, stdenv, socat, fetchFromGitHub, makeWrapper }:
+{
+  lib,
+  fetchFromGitHub,
+  makeWrapper,
+  mpv,
+  socat,
+  stdenvNoCC,
+}:
 
-stdenv.mkDerivation rec {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "mpvc";
   version = "1.3";
 
   src = fetchFromGitHub {
-    owner = "lwilletts";
+    owner = "gmt4";
     repo = "mpvc";
-    rev = version;
-    sha256 = "sha256-wPETEG0BtNBEj3ZyP70byLzIP+NMUKbnjQ+kdvrvK3s=";
+    rev = finalAttrs.version;
+    hash = "sha256-wPETEG0BtNBEj3ZyP70byLzIP+NMUKbnjQ+kdvrvK3s=";
   };
 
-  makeFlags = [ "PREFIX=$(out)" ];
-  installFlags = [ "PREFIX=$(out)" ];
-
-  postInstall = ''
-    wrapProgram $out/bin/mpvc --prefix PATH : "${socat}/bin/"
+  postPatch = ''
+    patchShebangs mpvc extras/mpvc-*
   '';
 
-  nativeBuildInputs = [ makeWrapper ];
-  buildInputs = [ socat ];
+  outputs = [ "out" "doc" ];
 
-  meta = with lib; {
+  buildInputs = [
+    mpv
+    socat
+  ];
+
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  installFlags = [
+    "PREFIX=${placeholder "out"}"
+    "DOCDIR=${placeholder "doc"}/share/doc/"
+  ];
+
+  dontConfigure = true;
+  dontBuild = true;
+
+  strictDeps = true;
+
+  preInstall = ''
+    mkdir -p $out $doc/share/doc/
+  '';
+
+  postFixup = ''
+    wrapProgram $out/bin/mpvc \
+      --prefix PATH : "${lib.makeBinPath [ mpv socat ]}"
+  '';
+
+  meta = {
+    homepage = "https://gmt4.github.io/mpvc/";
     description = "A mpc-like control interface for mpv";
-    mainProgram = "mpvc";
-    homepage = "https://github.com/lwilletts/mpvc";
-    license = licenses.mit;
-    maintainers = [ maintainers.neeasade ];
-    platforms = platforms.linux;
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ AndersonTorres neeasade ];
+    platforms = lib.platforms.linux;
   };
-}
+})
