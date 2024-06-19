d0cc59fad687268793195a7cd6b584edebfb530f
Anderson Torres <torres.anderson.85@protonmail.com>
2024-06-19 12:51:40 -0300

    qmplay2: 24.04.07 -> 24.05.23
    Now with two possible flavors: Qt5 and Qt6!
    And using Ninja as backend for Cmake (as required by upstream).

--- pkgs/by-name/qm/qmplay2/sources.nix
+++ pkgs/by-name/qm/qmplay2/sources.nix
@@ -6,14 +6,14 @@
   qmplay2 = let
     self = {
       pname = "qmplay2";
-      version = "24.04.07";
+      version = "24.05.23";
 
       src = fetchFromGitHub {
         owner = "zaps166";
         repo = "QMPlay2";
         rev = self.version;
         fetchSubmodules = true;
-        hash = "sha256-WIDGApvl+aaB3Vdv0sHY+FHWqzreWWd3/xOLV11YfxM=";
+        hash = "sha256-HFNq/t0lLVVmCR2tebigDg+X5E3s4a9fVgsjioeNzrg=";
       };
     };
   in
