5165aabecb8a62741054780c307f6c4de9a605e6
Anderson Torres <torres.anderson.85@protonmail.com>
2024-06-19 11:51:58 -0300

    qmplay2: 24.05.23 -> 24.06.16

--- pkgs/by-name/qm/qmplay2/sources.nix
+++ pkgs/by-name/qm/qmplay2/sources.nix
@@ -6,14 +6,14 @@
   qmplay2 = let
     self = {
       pname = "qmplay2";
-      version = "24.05.23";
+      version = "24.06.16";
 
       src = fetchFromGitHub {
         owner = "zaps166";
         repo = "QMPlay2";
         rev = self.version;
         fetchSubmodules = true;
-        hash = "sha256-HFNq/t0lLVVmCR2tebigDg+X5E3s4a9fVgsjioeNzrg=";
+        hash = "sha256-ECDEyF16GwIKn1raG7J56HFMYk4G6ezwAG2BEy8hUZs=";
       };
     };
   in
